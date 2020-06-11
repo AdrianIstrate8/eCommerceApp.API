c8a0ac679379c7470042ee38987d5b36d31fbcef
Adrian <majunior24@gmail.com>
2020-06-11 17:37:25 +0300

    End of section 6

--- eCommerceApp.API/Controllers/ProductsController.cs
+++ eCommerceApp.API/Controllers/ProductsController.cs
@@ -13,6 +13,7 @@ using eCommerceApp.API.Dtos;
 using AutoMapper;
 using eCommerceApp.API.Errors;
 using Microsoft.AspNetCore.Http;
+using eCommerceApp.API.Helpers;
 
 namespace eCommerceApp.API.Controllers
 {
@@ -35,13 +36,18 @@ namespace eCommerceApp.API.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts()
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery] ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification();
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+            var totalItems = await _productsRepo.CountAsync(countSpec);
+
+            var totalPages = Math.Ceiling((double)totalItems / (double)productParams.PageSize);
 
             var products = await _productsRepo.ListAsync(spec);
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
 
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, (int)totalPages, totalItems, data));
         }
 
         [HttpGet("{id}")]
---
+++ eCommerceApp.API/Helpers/Pagination.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceApp.API.Helpers
+{
+    public class Pagination<T> where T : class
+    {
+        public Pagination(int pageIndex, int pageSize, int pageCount, int itemCount, IReadOnlyList<T> data)
+        {
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            PageCount = pageCount;
+            ItemCount = itemCount;
+            Data = data;
+        }
+
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+        public int ItemCount { get; set; }
+        public IReadOnlyList<T> Data { get; set; }
+    }
+}
